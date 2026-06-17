29945dd29cd7d356f9da7c88913cfd09470723f8
Callisto95 <luluklzde@gmail.com>
2026-06-17 11:22:28 +0200

    update Config, use TypeAdapter

--- src/main/java/net/unknownuser/letitrain/ConfigDeserializer.java
+++ src/main/java/net/unknownuser/letitrain/ConfigDeserializer.java
@@ -1,38 +1,68 @@
 package net.unknownuser.letitrain;
 
 import com.google.gson.*;
+import com.google.gson.stream.*;
 
-import java.lang.reflect.*;
+import java.io.*;
 
-// if this does not exist, Gson uses type defaults for each variable
-// I hate this
-// just use Jackson.
-
-// This is technically not needed, but I hate whatever Gson is doing too much to just keep it
-public class ConfigDeserializer implements JsonDeserializer<Config> {
-	@Override
-	public Config deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
-		throws JsonParseException {
-		JsonObject  object = jsonElement.getAsJsonObject();
-		FieldGetter getter = new FieldGetter(object);
-		
-		int     keepRainChance      = getter.getInt("keepRainChance", Config.Defaults.KEEP_RAIN_CHANCE);
-		int     keepThunderChance   = getter.getInt("keepThunderChance", Config.Defaults.KEEP_THUNDER_CHANCE);
-		boolean preserveWeatherTime = getter.getBool("preserveWeatherTime", Config.Defaults.PRESERVE_WEATHER_TIME);
-		boolean logRolls            = getter.getBool("logRolls", Config.Defaults.LOG_ROLLS);
-		boolean resetThunderOnSleep = getter.getBool("resetThunderOnSleep", Config.Defaults.RESET_THUNDER_ON_SLEEP);
-		
-		Config config = new Config(keepRainChance,
-			keepThunderChance,
-			preserveWeatherTime,
-			logRolls,
-			resetThunderOnSleep
-		);
-		
-		if (getter.hasError()) {
-			config.hasError();
-		}
-		
-		return config;
-	}
+public class ConfigDeserializer extends TypeAdapter<Config> {
+    @Override
+    public void write(final JsonWriter writer, final Config config) throws IOException {
+        if (config == null) {
+            writer.nullValue();
+            return;
+        }
+        
+        writer.setFormattingStyle(FormattingStyle.PRETTY);
+        
+        writer.beginObject();
+        
+        writer
+            .name("keepRainChance")
+            .value(config.keepRainChance)
+            .name("keepThunderChance")
+            .value(config.keepThunderChance)
+            .name("preserveWeatherTime")
+            .value(config.preserveWeatherTime)
+            .name("logRolls")
+            .value(config.logRolls)
+            .name("resetThunderOnSleep")
+            .value(config.resetThunderOnSleep);
+        
+        writer.endObject();
+    }
+    
+    @Override
+    public Config read(final JsonReader reader) throws IOException {
+        int     keepRainChance      = Config.Defaults.KEEP_RAIN_CHANCE;
+        int     keepThunderChance   = Config.Defaults.KEEP_THUNDER_CHANCE;
+        boolean preserveWeatherTime = Config.Defaults.PRESERVE_WEATHER_TIME;
+        boolean logRolls            = Config.Defaults.LOG_ROLLS;
+        boolean resetThunderOnSleep = Config.Defaults.RESET_THUNDER_ON_SLEEP;
+        
+        reader.beginObject();
+        
+        while (reader.hasNext()) {
+            final String fieldName = reader.nextName();
+            
+            switch (fieldName) {
+                case "keepRainChance" -> keepRainChance = reader.nextInt();
+                case "keepThunderChance" -> keepThunderChance = reader.nextInt();
+                case "preserveWeatherTime" -> preserveWeatherTime = reader.nextBoolean();
+                case "logRolls" -> logRolls = reader.nextBoolean();
+                case "resetThunderOnSleep" -> resetThunderOnSleep = reader.nextBoolean();
+                default -> LetItRain.LOGGER.warn("Unknown config key: {}", fieldName);
+            }
+        }
+        
+        reader.endObject();
+        
+        return new Config(
+            keepRainChance,
+            keepThunderChance,
+            preserveWeatherTime,
+            logRolls,
+            resetThunderOnSleep
+        );
+    }
 }
