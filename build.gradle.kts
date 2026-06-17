bdf2381cae9250a4729091d694d83cda97f0a46a
Callisto95 <luluklzde@gmail.com>
2026-06-17 11:41:34 +0200

    update to 26.X

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("net.fabricmc.fabric-loom-remap")
+    id("net.fabricmc.fabric-loom")
     id("maven-publish")
     id("org.jetbrains.kotlin.jvm") version "2.4.0"
     id("com.modrinth.minotaur") version "2.+"
@@ -32,12 +32,12 @@ loom {
 dependencies {
     // To change the versions see the gradle.properties file
     minecraft("com.mojang:minecraft:${providers.gradleProperty("minecraft_version").get()}")
-    mappings("net.fabricmc:yarn:${providers.gradleProperty("yarn_mappings").get()}:v2")
-    modImplementation("net.fabricmc:fabric-loader:${providers.gradleProperty("loader_version").get()}")
+
+    implementation("net.fabricmc:fabric-loader:${providers.gradleProperty("loader_version").get()}")
 
     // Fabric API. This is technically optional, but you probably want it anyway.
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
-    modImplementation("net.fabricmc:fabric-language-kotlin:${providers.gradleProperty("fabric_kotlin_version").get()}")
+    implementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
+    implementation("net.fabricmc:fabric-language-kotlin:${providers.gradleProperty("fabric_kotlin_version").get()}")
 }
 
 tasks.processResources {
@@ -50,12 +50,12 @@ tasks.processResources {
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.release = 21
+    options.release = 25
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_21
+        jvmTarget = JvmTarget.JVM_25
     }
 }
 
@@ -65,8 +65,8 @@ java {
     // If you remove this line, sources will not be generated.
     withSourcesJar()
 
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
 }
 
 tasks.jar {
@@ -105,18 +105,10 @@ modrinth {
 
     gameVersions.addAll(
         listOf(
-            "1.21",
-            "1.21.1",
-            "1.21.2",
-            "1.21.3",
-            "1.21.4",
-            "1.21.5",
-            "1.21.6",
-            "1.21.7",
-            "1.21.8",
-            "1.21.9",
-            "1.21.10",
-            "1.21.11"
+            "26.1",
+            "26.1.1",
+            "26.1.2",
+            "26.2",
         )
     )
 
--- src/main/java/net/unknownuser/letitrain/mixins/ServerWorldMixin.java
+++ src/main/java/net/unknownuser/letitrain/mixins/ServerWorldMixin.java
@@ -1,7 +1,7 @@
 package net.unknownuser.letitrain.mixins;
 
-import net.minecraft.server.world.*;
-import net.minecraft.world.level.*;
+import net.minecraft.server.level.*;
+import net.minecraft.world.level.saveddata.*;
 import net.unknownuser.letitrain.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
@@ -9,34 +9,35 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 
 import static net.unknownuser.letitrain.LetItRain.*;
 
-@Mixin(ServerWorld.class)
-public class ServerWorldMixin {
+@Mixin(ServerLevel.class)
+public abstract class ServerWorldMixin {
+    
+    @Shadow
+    public abstract WeatherData getWeatherData();
     
     @Unique
     private static final int MAX_ROLL_CHANCE = 100;
     
-    @Final
-    @Shadow
-    private ServerWorldProperties worldProperties;
-    
     // as far as I can tell, resetWeather is only called when sleeping
-    @Inject(at = @At("HEAD"), method = "resetWeather", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "resetWeatherCycle", cancellable = true)
     private void resetWeather(CallbackInfo ci) {
-        if (worldProperties.isRaining()) {
+        final WeatherData weatherData = this.getWeatherData();
+        
+        if (weatherData.isRaining()) {
             int rainContinuationChance = Config.keepRainChance();
             int rainRoll               = RANDOM.nextInt(MAX_ROLL_CHANCE);
             
             logRoll("Rain continuation rolled: {}/{}", rainRoll, rainContinuationChance);
             
             if (rainRoll < rainContinuationChance) {
-                worldProperties.setRaining(true);
+                weatherData.setRaining(true);
                 logRoll("Rain continuation passed");
                 
                 if (Config.resetThunderOnSleep()) {
                     logRoll("Reset thundering because of configuration");
-                    worldProperties.setThundering(false);
-                    worldProperties.setThunderTime(0);
-                } else if (worldProperties.isThundering()) {
+                    weatherData.setThundering(false);
+                    weatherData.setThunderTime(0);
+                } else if (weatherData.isThundering()) {
                     int thunderContinuationChance = Config.keepThunderChance();
                     int thunderRoll               = RANDOM.nextInt(MAX_ROLL_CHANCE);
                     logRoll(
@@ -46,7 +47,7 @@ public class ServerWorldMixin {
                     );
                     
                     if (thunderRoll < thunderContinuationChance) {
-                        worldProperties.setThundering(true);
+                        weatherData.setThundering(true);
                         logRoll("Thunder continuation passed");
                     } else {
                         LetItRain.LOGGER.info("Thunder continuation not passed");
